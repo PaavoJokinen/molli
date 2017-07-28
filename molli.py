488586ffefb9c2c494c97ba045ceace24e1abbbd
Paavo Jokinen <paavo.jokinen@tut.fi>
2017-07-28 09:54:08 +0300

    ============= Modifications
      WordsToAdd.txt: Just few new words
      dictionary.xml: Just few new words
      molli.py: No changes really

--- molli.py
+++ molli.py
@@ -4,12 +4,22 @@ tree = ET.parse('dictionary.xml')
 root = tree.getroot()
 print(root.tag)
 
-for child in root:
-  print(child.tag, child.attrib)
-  for grandchild in child:
-    print(grandchild.tag)
-
-print(len(root.getchildren()))
+for word in root.findall('Word'):
+  
+#for child in root:
+#  print(child.tag, child.attrib)
+#  for grandchild in child:
+#    print(grandchild.tag)
+#
+#print(len(root.getchildren()))
 
 
 #print(Word.tag)
+
+
+#Tarvittavia funktioita
+#Hae sanaluokat
+#Hae sanojen määrä
+#Hae luokkakohtaiste määrät
+#Hae tarjolla olevat kielet
+#Hae kielivaihtoehtojen "mahtavuus"
